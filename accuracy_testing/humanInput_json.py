d6e17f60e2345a54f484e42efa08f349720e0713
DanielFernández <n231bb@gmail.com>
2021-04-26 04:32:45 -0700

    Output csv to json with basic cleaning and standardization to 'uuid' and 'gender' column

---
+++ accuracy_testing/humanInput_json.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+file_name = ''
+
+test_data = pd.read_csv(file_name)
+test_data.columns = ['old_idx', 'uuid', 'aws_link', 'data_entry_name',
+                     'application', 'date', 'country_of_origin',
+                     'panel_members', 'outcome', 'protected_grounds',
+                     'based_violence', 'keywords', 'references',
+                     'gender', 'indigenous', 'applicant_language',
+                     'is_credible',	'is_one_year',	'city_origin',
+                     'state_origin', 'notes_random', 'empty', 'empty1']
+
+test_data.drop(['old_idx', 'empty', 'empty1', 'data_entry_name', 'aws_link', 'notes_random'],
+    axis=1, inplace=True)
+
+
+# Remove '.pdf' from UUID
+def trim_pdf(s):
+    return s[:-4]
+
+
+# Standardize for later analysis
+def gender_prep(s):
+    try:
+        if s == 'n/a':
+            return ''
+        return s.lower()
+    except AttributeError:
+        return ''
+
+
+# Apply '.pdf' removal to uuid column
+test_data['uuid'] = test_data['uuid'].apply(trim_pdf)
+# Apply empty string to n/a values
+test_data['gender'] = test_data['gender'].apply(gender_prep)
+
+
+json_file = test_data.to_json(orient='records')
+
+
+with open('human_ocr.json', 'w') as f:
+    f.write(json_file)
+
